5b70364296b510e754a2d01c11fb871cbcfa1717
Nandha Kumar <76443408+nandha-batzzy@users.noreply.github.com>
2024-04-28 17:29:42 +0530

    model evaluation executed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,12 @@
 from cnnClassifier.constants import *
-from cnnClassifier.utils.common import read_yaml,create_directories
-from cnnClassifier.enitity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
+from cnnClassifier.utils.common import read_yaml,create_directories, save_json
+from cnnClassifier.constants import *
+from cnnClassifier.enitity.config_entity import (DataIngestionConfig, 
+                                                PrepareBaseModelConfig, 
+                                                PrepareCallbacksConfig, 
+                                                TrainingConfig,
+                                                EvaluationConfig)
+
 from pathlib import Path
 import os
 
@@ -86,3 +92,13 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config

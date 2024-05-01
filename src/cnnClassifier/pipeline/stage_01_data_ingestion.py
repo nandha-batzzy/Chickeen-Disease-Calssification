ca7f6dc79aacd11d39f6a4f567d1cf721c1674aa
Nandha Kumar <76443408+nandha-batzzy@users.noreply.github.com>
2024-05-01 13:30:48 +0530

    DVC file added

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -2,7 +2,6 @@ from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier import logger
 
-
 STAGE_NAME = "Data Ingestion stage"
 
 class DataIngestionTrainingPipeline:
